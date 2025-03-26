9fd79fce4f4155cdb5f48da82a56c06f43f560c4
armaunder <aidan@skule.ca>
2025-03-26 18:24:39 -0400

    fix

--- mie443_contest3/src/contest3.cpp
+++ mie443_contest3/src/contest3.cpp
@@ -9,9 +9,9 @@ using namespace std;
 // Global variables
 geometry_msgs::Twist follow_cmd;
 geometry_msgs::Twist vel; // ✅ Ensure vel is global
-sound_play::SoundClient sc; // ✅ Make sc global
 string path_to_sounds; // ✅ Make sound path global
 ros::Publisher vel_pub; // ✅ Make publisher global
+sound_play::SoundClient sc; // ✅ Make sc global
 
 uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
 uint8_t leftstate = bumper[kobuki_msgs::BumperEvent::LEFT];
@@ -33,85 +33,54 @@ void bumperCB(const kobuki_msgs::BumperEvent::ConstPtr& msg) {
 
 //-------------------------------------------------------------
 // ✅ Fixed `scared()` function to use global `vel` and `sc`
-// void scared() {
-//     sc.playWave(path_to_sounds + "r2scream.wav");
-//     sleep(2.0);
-//     sc.stopWave(path_to_sounds + "r2scream.wav");
-
-//     vel.linear.x = -2;
-//     vel.angular.z = 1;
-//     vel_pub.publish(vel);
-// }
-
-// // ✅ Happy function (unchanged)
-// void happy() {
-//     sc.playWave(path_to_sounds + "r2scream.wav"); // Change sound
-//     sleep(2.0);
-//     vel.linear.x = 2;
-//     vel_pub.publish(vel);
-//     sleep(2.0);
-//     vel.linear.x = 0;
-//     vel_pub.publish(vel);
-// }
-
-// human gets too close, runs away
-void scared(){
-	// sc.playWave(path_to_sounds+"r2scream.wav");
-	vel.linear.x = -2;
-	vel.angular.z = 1;
-	vel_pub.publish(vel);
-    // sleep(2.0);
-	// sc.stopWave(path_to_sounds+"r2scream.wav");
+void scared() {
+    vel.linear.x = -2;
+    vel.angular.z = 1;
+    vel_pub.publish(vel);
 }
 
 // gets picked up, wheels spin fast while it is in the air
 void happy(){
-	// sc.playWave(path_to_sounds+"r2scream.wav"); //change sound
-	// sleep(2.0);
-	vel.linear.x = 2;
-	vel_pub.publish(vel);
-	sleep(2.0);
-	vel.linear.x = 0;
-	vel_pub.publish(vel); 
+    vel.linear.x = 2;
+    vel_pub.publish(vel);
+    sleep(2.0);
+    vel.linear.x = 0;
+    vel_pub.publish(vel); 
 }
 
 // finds human, sound of shock
 void surprised(){
-	// sc.playWave(path_to_sounds+"r2scream.wav"); //change sound
-	// sleep(2.0); 
 }
 
 // hits bumper back up and start spinning
 void anger(){
-	// sc.playWave(path_to_sounds+"r2scream.wav"); //change sound
-	//sleep(2.0);
-	vel.linear.x = -2;
-	vel_pub.publish(vel);
-	vel.linear.x = 0;
-	vel.angular.z = 1;
-	vel_pub.publish(vel);
-	sleep(2.0);
-	vel.angular.x = 0;
-	vel_pub.publish(vel); 
+    vel.linear.x = -2;
+    vel_pub.publish(vel);
+    vel.linear.x = 0;
+    vel.angular.z = 1;
+    vel_pub.publish(vel);
+    sleep(2.0);
+    vel.angular.x = 0;
+    vel_pub.publish(vel); 
 }
 
 // loses human, starts meandering
 void sad(){
-	vel.linear.x = 0.5;
-	vel.angular.z = 0.5;
-	vel_pub.publish(vel);
-	// ros::Duration(1.0).sleep();
+    vel.linear.x = 0.5;
+    vel.angular.z = 0.5;
+    vel_pub.publish(vel);
 }
 
 //-------------------------------------------------------------
 int main(int argc, char **argv) {
     ros::init(argc, argv, "image_listener");
-    ros::Duration(1.0).sleep(); // Added delay for ROS initialization
     ros::NodeHandle nh;
+    ros::WallDuration(1.0).sleep(); // Added delay for ROS initialization
 
     // ✅ Initialize global variables
     path_to_sounds = ros::package::getPath("mie443_contest3") + "/sounds/";
     vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
+    sc = sound_play::SoundClient(nh);
 
     // Subscribers
     ros::Subscriber follower = nh.subscribe("follower_velocity_smoother/smooth_cmd_vel", 10, &followerCB);
@@ -137,41 +106,27 @@ int main(int argc, char **argv) {
     ros::Duration(0.5).sleep();
 
     while(ros::ok() && secondsElapsed <= 480){		
-		ros::spinOnce();
-		// bumperCB();	
-		// humanCB();	
-		// pickedUpCB();
-		// foundCB();
-		// lostCB();
-		// followerCB();
+        ros::spinOnce();
 
-
-		if(world_state == 0){
-			//fill with your code
-			//vel_pub.publish(vel);
-			vel_pub.publish(follow_cmd);
-
-		}else if(world_state == 1){
+        if(world_state == 0){
+            vel_pub.publish(follow_cmd);
+        } else if(world_state == 1){
             sc.playWave(path_to_sounds + "r2scream.wav");
-			scared();
+            scared();
             ros::Duration(2.0).sleep();
             sc.stopWave(path_to_sounds + "r2scream.wav");
-		}
-		else if(world_state == 2){
-			anger();
-		}
-		else if(world_state == 3){
-			happy();
-		}
-		else if(world_state == 4){
-			surprised();
-		}
-		else if(world_state == 5){
-			sad();
-		}
-		secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
-		loop_rate.sleep();
-	}
+        } else if(world_state == 2){
+            anger();
+        } else if(world_state == 3){
+            happy();
+        } else if(world_state == 4){
+            surprised();
+        } else if(world_state == 5){
+            sad();
+        }
+        secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
+        loop_rate.sleep();
+    }
 
-	return 0;
+    return 0;
 }
