a357cbdad723b71c497239f58ddd6d70d4efb003
armaunder <aidan@skule.ca>
2025-03-30 20:30:49 -0400

    amélie upload

--- mie443_contest3/src/contest3.cpp
+++ mie443_contest3/src/contest3.cpp
@@ -4,8 +4,10 @@
 #include <chrono>
 #include <kobuki_msgs/BumperEvent.h>
 
+
 using namespace std;
 #define N_BUMPER (3)
+
 
 // Global variables
 geometry_msgs::Twist follow_cmd;
@@ -13,131 +15,164 @@ geometry_msgs::Twist vel; // ✅ Ensure vel is global
 string path_to_sounds; // ✅ Make sound path global
 ros::Publisher vel_pub; // ✅ Make publisher global
 
+
 uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
-uint8_t leftstate = bumper[kobuki_msgs::BumperEvent::LEFT];
-uint8_t frontstate = bumper [kobuki_msgs::BumperEvent::CENTER];
-uint8_t rightstate = bumper [kobuki_msgs::BumperEvent::RIGHT];
+// uint8_t leftstate = bumper[kobuki_msgs::BumperEvent::LEFT];
+// uint8_t frontstate = bumper[kobuki_msgs::BumperEvent::CENTER];
+// uint8_t rightstate = bumper[kobuki_msgs::BumperEvent::RIGHT];
+
 
 int world_state;
 
+
 void followerCB(const geometry_msgs::Twist msg) {
-    follow_cmd = msg;
+   follow_cmd = msg;
 }
+
 
 void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
 {
-    bumper[msg->bumper] = msg->state;
+   bumper[msg->bumper] = msg->state;
+   // uint8_t leftstate = bumper[kobuki_msgs::BumperEvent::LEFT];
+   // uint8_t frontstate = bumper[kobuki_msgs::BumperEvent::CENTER];
+   // uint8_t rightstate = bumper[kobuki_msgs::BumperEvent::RIGHT];
+
+
+   if (leftstate == kobuki_msgs::BumperEvent::PRESSED || frontstate == kobuki_msgs::BumperEvent::PRESSED || rightstate == kobuki_msgs::BumperEvent::PRESSED) {
+       world_state = 1;
+   }
 }
+
 
 // human gets too close, runs away
 void scared(){
-	vel.linear.x = -2;
-	vel.angular.z = 1;
-	vel_pub.publish(vel);
+   vel.linear.x = -2;
+   vel.angular.z = 1;
+   vel_pub.publish(vel);
+
 
 }
+
 
 // gets picked up, wheels spin fast while it is in the air
 void happy(){
-	vel.linear.x = 2;
-	vel_pub.publish(vel);
-	vel.linear.x = 0;
-	vel_pub.publish(vel); 
+   vel.linear.x = 2;
+   vel_pub.publish(vel);
+   vel.linear.x = 0;
+   vel_pub.publish(vel);
 }
+
 
 // finds human, sound of shock
 void surprised(){
-	// sc.playWave(path_to_sounds+"r2scream.wav"); //change sound
-	// sleep(2.0); 
+   // sc.playWave(path_to_sounds+"r2scream.wav"); //change sound
+   // sleep(2.0);
 }
+
 
 // hits bumper back up and start spinning
 void anger(){
-	// sc.playWave(path_to_sounds+"r2scream.wav"); //change sound
-	//sleep(2.0);
-    sc.playWave(path_to_sounds + "sound.wav");
-	vel.linear.x = -2;
-	vel_pub.publish(vel);
-	vel.linear.x = 0;
-	vel.angular.z = 1;
-	vel_pub.publish(vel);
-	sleep(2.0);
-	vel.angular.x = 0;
-	vel_pub.publish(vel); 
+   // sc.playWave(path_to_sounds+"r2scream.wav"); //change sound
+   //sleep(2.0);
+   sc.playWave(path_to_sounds + "sound.wav");
+   vel.linear.x = -2;
+   vel_pub.publish(vel);
+   vel.linear.x = 0;
+   vel.angular.z = 1;
+   vel_pub.publish(vel);
+   sleep(2.0);
+   vel.angular.x = 0;
+   vel_pub.publish(vel);
 }
+
 
 // loses human, starts meandering
 void sad(){
-	vel.linear.x = 0.5;
-	vel.angular.z = 0.5;
-	vel_pub.publish(vel);
+   vel.linear.x = 0.5;
+   vel.angular.z = 0.5;
+   vel_pub.publish(vel);
 }
+
 
 //-------------------------------------------------------------
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "image_listener");
-    ros::NodeHandle nh;
+   ros::init(argc, argv, "image_listener");
+   ros::NodeHandle nh;
 
-    // ✅ Initialize global variables
-    path_to_sounds = ros::package::getPath("mie443_contest3") + "/sounds/";
-    sound_play::SoundClient sc; // Initialize sound client after NodeHandle
-    vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
 
-    // Subscribers
-    ros::Subscriber follower = nh.subscribe("follower_velocity_smoother/smooth_cmd_vel", 10, &followerCB);
-    ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCB);
+   // ✅ Initialize global variables
+   path_to_sounds = ros::package::getPath("mie443_contest3") + "/sounds/";
+   sound_play::SoundClient sc; // Initialize sound client after NodeHandle
+   vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
 
-    // Contest count down timer
-    ros::Rate loop_rate(10);
-    std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
-    uint64_t secondsElapsed = 0;
 
-    // Image Transport
-    imageTransporter rgbTransport("camera/image/", sensor_msgs::image_encodings::BGR8);
-    imageTransporter depthTransport("camera/depth_registered/image_raw", sensor_msgs::image_encodings::TYPE_32FC1);
+   // Subscribers
+   ros::Subscriber follower = nh.subscribe("follower_velocity_smoother/smooth_cmd_vel", 10, &followerCB);
+   ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCB);
 
-    world_state = 0;
-    double angular = 0.2;
-    double linear = 0.25;
 
-    vel.angular.z = angular;
-    vel.linear.x = linear;
+   // Contest count down timer
+   ros::Rate loop_rate(10);
+   std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+   uint64_t secondsElapsed = 0;
 
-    sc.playWave(path_to_sounds + "sound.wav");
-    ros::Duration(0.5).sleep();
-	ros::Rate loop_rate(10);
-    while(ros::ok() && secondsElapsed <= 480){		
-		ros::spinOnce();
-		
-		bool any_bumper_pressed=false;
-        for (uint32_t b_idx = 0; b_idx < N_BUMPER; ++b_idx) {
-        	any_bumper_pressed |= (bumper[b_idx] == kobuki_msgs::BumperEvent::PRESSED);
-        }
 
-		if (any_bumper_pressed){
-			world_state = 1;
-		}
-		if(world_state == 0){
-			vel_pub.publish(follow_cmd);
+   // Image Transport
+   imageTransporter rgbTransport("camera/image/", sensor_msgs::image_encodings::BGR8);
+   imageTransporter depthTransport("camera/depth_registered/image_raw", sensor_msgs::image_encodings::TYPE_32FC1);
 
-		}else if(world_state == 1){
-			anger();
-		}
-		else if(world_state == 2){
-			scared();
-		}
-		else if(world_state == 3){
-			happy();
-		}
-		else if(world_state == 4){
-			surprised();
-		}
-		else if(world_state == 5){
-			sad();
-		}
-		secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
-		loop_rate.sleep();
-	}
 
-	return 0;
+   world_state = 0;
+   double angular = 0.2;
+   double linear = 0.25;
+
+
+   vel.angular.z = angular;
+   vel.linear.x = linear;
+
+
+   sc.playWave(path_to_sounds + "sound.wav");
+   ros::Duration(0.5).sleep();
+   ros::Rate loop_rate(10);
+   while(ros::ok() && secondsElapsed <= 480){     
+       ros::spinOnce();
+      
+       bool any_bumper_pressed=false;
+       for (uint32_t b_idx = 0; b_idx < N_BUMPER; ++b_idx) {
+           any_bumper_pressed |= (bumper[b_idx] == kobuki_msgs::BumperEvent::PRESSED);
+       }
+
+
+       if (any_bumper_pressed){
+           world_state = 1;
+       }
+       if(world_state == 0){
+           vel_pub.publish(follow_cmd);
+
+
+       }else if(world_state == 1){
+           anger();
+           ROS_INFO("Bumper hit");
+           ROS_INFO("Anger");
+           sc.stopWave(path_to_sounds+"r2scream.wav");
+       } // human gets too close, scared
+       else if(world_state == 2){
+           scared();
+       }
+       else if(world_state == 3){
+           happy();
+       }
+       else if(world_state == 4){
+           surprised();
+       }
+       else if(world_state == 5){
+           sad();
+       }
+       secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
+       loop_rate.sleep();
+   }
+
+
+   return 0;
 }
+
